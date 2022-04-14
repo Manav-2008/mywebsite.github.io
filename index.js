17bcd637c3e79f66d8ef4cf492eb4e8c9070e1cb
Manav-2008 <102307557+Manav-2008@users.noreply.github.com>
2022-04-14 17:49:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+console.log("hello World", 10 + 10)
+    // document.write("This is My Website")
+var a = 40;
+var b = 20;
+console.log(a + b, "This is Maths")
+var c = true;
+var d = false;
+console.log("true and false", c, d)
+    // function percent( x , y){
+    //     z = (y / x);
+    //     return z;
+
+// }
+// z1 = percent(10, 100)
+// console.log(z1);
+// z2 = percent(25, 100);
+// console.log(z2)
+function sintrest(x, y, z) {
+    w = (x * y * z) / 100;
+    return w
+}
+w1 = sintrest(8000, 5, 2);
+console.log(w1)
+w2 = sintrest(5000, 10, 5);
+console.log(w2)
+var age = 4;
+if (age > 8) {
+    console.log("you are not a kid")
+} else {
+    console.log("you are a kid")
+}
+var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+console.log(arr)
+for (var i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+}
